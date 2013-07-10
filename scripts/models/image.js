c1395555e23e801f2260ad8406164ccd0ee7fa14
AchintyaAshok <achintyaashok92@gmail.com>
2013-07-10 12:03:03 -0400

    changed image initialize function

---
+++ scripts/models/image.js
@@ -0,0 +1,19 @@
+define(['backbone'],
+	function(Backbone){
+		
+		var Image = Backbone.Model.extend({
+
+			initialize: function(options){
+				this.image_id = options.image_id;
+				this.image_url = options.image_url;
+				this.width = options.width;
+				this.height = options.height;
+				this.caption = options.caption;
+				this.credit = options.credit;
+			},
+
+			idAttribute = "image_id"
+
+		});
+	}
+);
